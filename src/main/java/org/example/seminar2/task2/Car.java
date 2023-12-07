94e130fb62cbc9fc01aeb63e38ee606fbad34840
julemalysheva <julemalysheva@gmail.com>
2023-12-07 12:26:13 +0500

    seminar2 java junior reflection API

---
+++ src/main/java/org/example/seminar2/task2/Car.java
@@ -0,0 +1,16 @@
+package org.example.seminar2.task2;
+
+public class Car {
+
+    private String model;
+
+    private String color;
+
+    private int year;
+
+    public Car(String model, String color, int year) {
+        this.model = model;
+        this.color = color;
+        this.year = year;
+    }
+}
