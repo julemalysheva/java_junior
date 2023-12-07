0b4ef3b236195d048ba7ee608948bba3dd9a197f
julemalysheva <julemalysheva@gmail.com>
2023-12-07 12:47:22 +0500

    seminar1 add cardBalancing() variant2

--- src/main/java/org/example/seminar1/task2/Cart.java
+++ src/main/java/org/example/seminar1/task2/Cart.java
@@ -104,7 +104,7 @@ public class Cart<T extends Food> {
 
         if (!proteins.get()) {
             things.stream()
-                    .filter(thing -> thing.getProteins())
+                    .filter(Food::getProteins)
                     .findFirst()
                     .ifPresent(food -> {
                         proteins.set(true);
@@ -114,7 +114,7 @@ public class Cart<T extends Food> {
 
         if (!fats.get()) {
             things.stream()
-                    .filter(thing -> thing.getFats())
+                    .filter(Food::getFats)
                     .findFirst()
                     .ifPresent(food -> {
                         fats.set(true);
@@ -124,7 +124,7 @@ public class Cart<T extends Food> {
 
         if (!carbohydrates.get()) {
             things.stream()
-                    .filter(thing -> thing.getCarbohydrates())
+                    .filter(Food::getCarbohydrates)
                     .findFirst()
                     .ifPresent(food -> {
                         carbohydrates.set(true);
@@ -138,6 +138,72 @@ public class Cart<T extends Food> {
             System.out.println("Невозможно сбалансировать корзину по БЖУ.");
         }
     }
+
+    /**
+     * Решение Станислава
+     * @return
+     *
+     */
+    /**
+     * Балансировка корзины
+     */
+//    public void cardBalancing()
+//    {
+//        boolean proteins = checkNutrientFlag(Food::getProteins);
+//        boolean fats = checkNutrientFlag(Food::getFats);
+//        boolean carbohydrates = checkNutrientFlag(Food::getCarbohydrates);
+//
+//        if (proteins && fats && carbohydrates) {
+//            System.out.println("Корзина уже сбалансирована по БЖУ.");
+//            return;
+//        }
+//
+//        Collection<T> marketFoods = market.getThings(clazz);
+//        proteins = checkNutrientFlag(proteins, Food::getProteins, marketFoods);
+//        fats = checkNutrientFlag(fats, Food::getFats, marketFoods);
+//        carbohydrates = checkNutrientFlag(carbohydrates, Food::getCarbohydrates, marketFoods);
+//
+//        if (proteins && fats && carbohydrates) {
+//            System.out.println("Корзина сбалансирована по БЖУ.");
+//        } else {
+//            System.out.println("Невозможно сбалансировать корзину по БЖУ.");
+//        }
+//
+//    }
+//
+//    /**
+//     * Проверка наличия конкретного питательного элемента в корзине
+//     * @param nutrientCheck список продуктов в корзине
+//     * @return состояние обновленного флага питательного элемента
+//     */
+//    private boolean checkNutrientFlag(Predicate<Food> nutrientCheck) {
+//        Optional<T> optionalFood = foodstuffs.stream()
+//                .filter(nutrientCheck)
+//                .findFirst();
+//        return optionalFood.isPresent();
+//    }
+//
+//    /**
+//     * Поиск недостающих питательных элементов в корзине и добавление питательно элемента
+//     * исходя из общего фильтра продуктов
+//     * @param nutrientFlag наличие питательного элемента
+//     * @param nutrientCheck список продуктов в корзине
+//     * @param foods доступный список продуктов (исходя из текущего фильтра)
+//     * @return состояние обновленного флага питательного элемента (скорее всего будет true,
+//     * false - в случае, если невозможно найти продукт с нужным питательным элементом, в таком
+//     * случае, невозможно сбалансировать корзину по БЖУ
+//     */
+//    private boolean checkNutrientFlag(boolean nutrientFlag, Predicate<Food> nutrientCheck, Collection<T> foods) {
+//        if (!nutrientFlag) {
+//            Optional<T> optionalFood = foods.stream()
+//                    .filter(nutrientCheck)
+//                    .findFirst();
+//            optionalFood.ifPresent(foodstuffs::add);
+//            return optionalFood.isPresent();
+//        }
+//        return true;
+//    }
+
 
     public Collection<T> getFoodstuffs() {
         return foodstuffs;
